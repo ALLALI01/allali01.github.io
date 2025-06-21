4e9b1c0c279e2ef15c40d360691525b60ead2f8a
Aaron L <daeige@hotmail.com>
2025-06-20 23:30:29 -0400

    Added javascript fetch for projects page

---
+++ js/projects.js
@@ -0,0 +1,52 @@
+const yourUserNameHere = 'allali01'; // Replace with your GitHub username
+const aUserNameHere = 'allali01';
+
+async function getUser(ghUserName) {
+    try {
+        const res = await fetch(`https://api.github.com/users/${ghUserName}`);
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getUser(yourUserNameHere);
+
+async function getUserRepos(ghUserName) {
+    try {
+        const res = await fetch(`https://api.github.com/users/${ghUserName}/repos`)
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function populateRepos() {
+    getUserRepos(aUserNameHere);
+    const res = await fetch(`https://api.github.com/users/${aUserNameHere}/repos`);
+    const data = await res.json();
+    const repoList = document.getElementById('project-section');
+
+    data.forEach(repo => {
+        const listContainer = document.createElement('div');
+        listContainer.classList.add('repo-list-item');
+        const projectTitle = document.createElement('h2');
+        const projectLink = document.createElement('a');
+        const projectDescription = document.createElement('p');
+
+        projectLink.href = repo.html_url;
+        projectLink.textContent = repo.name;
+        projectLink.target = "_blank"; // optional: open in new tab
+
+        projectDescription.textContent = repo.description || 'No description available';
+
+        repoList.appendChild(listContainer);
+        listContainer.appendChild(projectTitle);
+        projectTitle.appendChild(projectLink);
+        listContainer.appendChild(projectDescription);
+    });
+}
+
+populateRepos();
